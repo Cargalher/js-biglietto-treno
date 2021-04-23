837d455e76e885827c0efc2ffdd9a46ad214e9fd
Cargalher <cargalher@gmail.com>
2021-04-23 15:22:18 +0200

    questions made

---
+++ main.js
@@ -0,0 +1,12 @@
+// calculate ticket price
+
+// Ask customer Number of kms
+var kilometers = prompt ('How far would you travel?');
+console.log('kilometers');
+
+// Ask customer Age
+var age = prompt ('How old are you?');
+console.log('age');
+// General Ticket price
+var tPrice = kilometers * 0.21;
+console.log('tPrice');
