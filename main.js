467482f8a8d8e5d747edf8e14e462b10bf021d49
Cargalher <cargalher@gmail.com>
2021-04-23 15:57:29 +0200

    console.log commented

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 // calculate ticket price
 
 // Ask customer Number of kms
-var kilometers = prompt ('How far would you travel?');
-console.log('kilometers');
+var kilometers = prompt('How far would you travel?');
+// console.log('kilometers');
 
 // Ask customer Age
-var age = prompt ('How old are you?');
-console.log('age');
+var age = prompt('How old are you?');
+// console.log('age');
+
 // General Ticket price
 var tPrice = kilometers * 0.21;
-console.log('tPrice');
+// console.log('tPrice');
 
 //Prices with and without Discounts
 
 if ( age < 18 ){
     tPrice = tPrice * 0.20;
-    document.getElementById ('info').innerHTML = 'The price of your ticket is :';
+    document.getElementById('info').innerHTML = 'The price of your ticket is :';
 }else if ( age > 65 ){
     tPrice= tPrice * 0.40;
-    document.getElementById ('info').innerHTML = 'The price of your ticket is :';
+    document.getElementById('info').innerHTML = 'The price of your ticket is :';
 }else{
-    document.getElementById ('info').innerHTML = tPrice;
+    document.getElementById('info').innerHTML = tPrice;
 }
