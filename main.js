418665451386b09cd83ae0b1705bdbc1913b251b
Cargalher <cargalher@gmail.com>
2021-04-26 10:19:46 +0200

    substrat disccount

--- main.js
+++ main.js
@@ -2,24 +2,24 @@
 
 // Ask customer Number of kms
 var kilometers = prompt('How far would you travel?');
-// console.log('kilometers');
+console.log(kilometers);
 
 // Ask customer Age
 var age = prompt('How old are you?');
-// console.log('age');
+console.log(age);
 
 // General Ticket price
 var tPrice = kilometers * 0.21;
-// console.log('tPrice');
+console.log(tPrice);
 
 //Prices with and without Discounts
 
 if ( age < 18 ){
-    tPrice = tPrice * 0.20;
+    tPrice -= tPrice * 0.20;
     var rounded_price = tPrice.toFixed(2);
     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
 }else if ( age > 65 ){
-    tPrice= tPrice * 0.40;
+    tPrice -= tPrice * 0.40;
     var rounded_price = tPrice.toFixed(2);
     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
 }else{
